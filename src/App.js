b0120e7b0448a650d7af4c1281de6066692eb8f7
Adeline Liou <adeline@usc.edu>
2021-11-15 13:08:32 -0800

    Updatedhome
    Home page updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-=======
 import logo from './images/logo.svg';
 import title from './images/title.svg';
 import enteruser from './images/enteruser.svg';
 import enterpwd from './images/enterpwd.svg';
 import loginsignup from './images/loginsignup.svg';
 import whiterect from './images/whiterect.svg';
->>>>>>> Stashed changes
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< Updated upstream
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-=======
         <img src={logo} className="App-logo" />
         <img src={title} className="App-name" />
         <img src={enteruser} className="App-items" />
@@ -28,7 +20,6 @@ function App() {
 
 
         {/* <p>
->>>>>>> Stashed changes
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -38,14 +29,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
       </header>
-<<<<<<< Updated upstream
-=======
       
       
      
->>>>>>> Stashed changes
     </div>
   );
 }
