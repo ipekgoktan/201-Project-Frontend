e6546cd0ef9d113f915b908e520e2dbb2e5bc903
Adeline Liou <adeline@usc.edu>
2021-12-02 16:26:24 -0800

    Update

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,8 +8,73 @@ import whiterect from './../images/whiterect.svg';
 import {Link } from "react-router-dom";
 
 import './App.css';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSFegd3hVP_DQ2P35AUldm8cq8BWUriHo",
+  authDomain: "u-of-scheduling-conflicts.firebaseapp.com",
+  projectId: "u-of-scheduling-conflicts",
+  storageBucket: "u-of-scheduling-conflicts.appspot.com",
+  messagingSenderId: "850021422271",
+  appId: "1:850021422271:web:db14a028e01540e462a20a",
+  measurementId: "G-NTV5ZYV97L"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+function tryy(){
+  //var userEmail = document.getElementlById("UsernameBox").value;
+  var userEmail = document.getElementById("UsernameBox").value;
+  var userPass = document.getElementById("PasswordBox").value;
+  const auth = getAuth();
+
+  // var axios = require('axios');
+  // var data = '';
+
+  // var config = {
+  //   method: 'post',
+  //   url: 'https://uofschedulingconflictsapi.herokuapp.com/api/account',
+  //   headers: { 
+  //     'Content-Type': 'application/json', 
+  //     'Authorization': 'Bearer <jwt token>'
+  //   },
+  //   data : data
+  // };
+
+  // axios(config)
+  // .then(function (response) {
+  //   console.log(JSON.stringify(response.data));
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
+
+  signInWithEmailAndPassword(auth, userEmail, userPass)
+    .then((userCredential) => {
+      //Signed in
+      const user = userCredential.user;
+      window.location.replace(`/Accmenu`)
+      window.alert(auth.getAuth);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("wrong Password" + errorMessage);
+    });
+
+}
 
 function App() {
+  // const [token, setToken] = useState();
+
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,36 +88,19 @@ function App() {
       
         <img src={enteruser} className="App-items" />
         <form class = "formLocation">
-          <input type="text" class = "InputBox"></input>
+          <input type="text" class = "InputBox" id="UsernameBox"></input>
         </form>
         <img src={enterpwd} className="App-items" />
         <form class = "formLocation">
-          <input type="text" class = "InputBox"></input>
+          <input type="text" class = "InputBox" id="PasswordBox"></input>
         </form>
 
-        <Link to={`/Accmenu`}>
-          <img src={login} className="App-items" />
-        </Link>
+        <img src={login} className="App-items" onClick={tryy} />
         <Link to={`/Signup`}>
           <img src={signup} className="App-items" />
         </Link>
 
-
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>*/}
       </header>
-      
-      
-     
     </div>
   );
 }
